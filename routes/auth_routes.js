49bf845789b09d0d6e8d94099e180c37a76ebec5
Tigran <tig.vardanyan.001@gmail.com>
2022-05-30 15:25:37 +0400

    end of login and register

--- routes/auth_routes.js
+++ routes/auth_routes.js
@@ -1,6 +1,8 @@
 const {Router} = require('express')
 const {check, validationResult} = require('express-validator')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const config = require('config')
 const User = require('../models/User')
 const router = Router()
 
@@ -16,7 +18,12 @@ router.post(
         const errors = validationResult(req)
 
         if (!errors.isEmpty()) {
-            return res.status(400).json({errors: errors.array(), message: "Not valid"})
+            return res.status(400).json(
+                {
+                    errors: errors.array(),
+                    message: "Not valid credentials"
+                }
+            )
         }
 
         const {email, password} = req.body;
@@ -35,12 +42,53 @@ router.post(
 
         res.status(200).json({message: "Success"})
     } catch (e) {
-        res.status(500).json({message: 'Something is wrong, try later'})
+        res.status(500).json({message: 'Something is wrong, try again'})
     }
 })
 // /api/auth/login
-router.post('/login', async (req, res) => {
+router.post(
+    '/login',
+    [
+        check('email', 'Enter correct login').normalizeEmail().isEmail(),
+        check('password', 'Enter password').exists()
+    ],
+    async (req, res) => {
+    try{
+        const errors = validationResult(req)
 
+        if (!errors.isEmpty()) {
+            return res.status(400).json(
+                {
+                    errors: errors.array(),
+                    message: "Not valid credentials while log in."
+                }
+            )
+        }
+
+        const {email, password} = req.body
+
+        const user = await User.findOne({email})
+
+        if (!user) {
+            return res.status(400).json({message: 'User does not exists'})
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password)
+
+        if (!isMatch) {
+            return res.status(400).json({message: 'Wrong password!'})
+        }
+
+        const token = jwt.sign(
+            {userId: user.id},
+            config.get('jwtSecret'),
+            {expiresIn: '1h'}
+        )
+
+        res.json({token, useId: user.id})
+    } catch (e) {
+        res.status(500).json({message: 'Something is wrong, try again'})
+    }
 })
 
 module.exports = router
